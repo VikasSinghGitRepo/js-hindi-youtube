9d6962b178de8d64f91275ae2dc8c38428378676
Vikas Singh <104720691+VikasSinghGitRepo@users.noreply.github.com>
2024-10-08 01:27:58 +0000

    container setup and variables and data types

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = "13432"
+let accountName = "vikas"
+var accountAge = 12
+let accountState; // undefined
+
+email="vikas@gmail.com"
+
+// accountId=1    --it is not allowed
+accountName="singh"
+email="singh@gmail.com"
+accountAge=13
+
+//use let instead of var always because of issue in block scope and functional scope
+
+console.log(accountId)
---
+++ 01_Basics/02_datattypes.js
@@ -0,0 +1,31 @@
+"use strict" //treat all code as newer version of javascript
+
+// alert(3+3)  -- it does not work because we are working in node and not in browser
+
+//important thing in coding is readability so always write such code that is always readable
+
+
+let name = "vikas" // string
+let age = 19 // number
+let isLoggedIn = true // boolean
+
+
+//--------------Primitive data types----------------//
+// number ==> 2 to power 53
+// bigint
+// string 
+//boolean ==> true or false
+//null  ( it is a standalone value as well as data type in javascript)
+//undefined
+//symbol ( used for uniqueness)
+
+
+console.log(typeof(null));//object 
+console.log(typeof(undefined));//undefined
+
+
+
+// difference between null and undefined
+
+
+
